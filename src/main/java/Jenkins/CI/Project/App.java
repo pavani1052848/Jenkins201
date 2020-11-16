41d3381d70c26e9ab5259e7cf63d46812ecb9079
pavani1052848 <b.nagapavani@mindtree.com>
2020-11-16 12:51:29 +0530

    To check build trigger

--- src/main/java/Jenkins/CI/Project/App.java
+++ src/main/java/Jenkins/CI/Project/App.java
@@ -4,6 +4,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Jenkins integration" );
+        System.out.println( "Jenkins integration 201" );
     }
 }
